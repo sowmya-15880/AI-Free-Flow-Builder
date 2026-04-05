4cccf3778feaf0a3a6e1844910e87a942fd624f8
sowmya-15880 <sowmyanarayanan.ss@zohocorp.com>
2026-04-05 19:40:39 +0530

    Trim frontend repo artifacts

--- rust/target/release/build/serde_json-c2ad7531314a436e/build_script_build-c2ad7531314a436e.d
+++
@@ -1,5 +0,0 @@
-/Users/sowmya-15880/FFB/app/rust/target/release/build/serde_json-c2ad7531314a436e/build_script_build-c2ad7531314a436e.d: /Users/sowmya-15880/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_json-1.0.149/build.rs
-
-/Users/sowmya-15880/FFB/app/rust/target/release/build/serde_json-c2ad7531314a436e/build_script_build-c2ad7531314a436e: /Users/sowmya-15880/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_json-1.0.149/build.rs
-
-/Users/sowmya-15880/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_json-1.0.149/build.rs:
--- rust/target/wasm32-unknown-unknown/release/deps/ffb_canvas_engine.d
+++
@@ -1,7 +0,0 @@
-/Users/sowmya-15880/FFB/app/rust/target/wasm32-unknown-unknown/release/deps/ffb_canvas_engine.d: ffb_canvas_engine/src/lib.rs
-
-/Users/sowmya-15880/FFB/app/rust/target/wasm32-unknown-unknown/release/deps/ffb_canvas_engine.wasm: ffb_canvas_engine/src/lib.rs
-
-/Users/sowmya-15880/FFB/app/rust/target/wasm32-unknown-unknown/release/deps/libffb_canvas_engine.rlib: ffb_canvas_engine/src/lib.rs
-
-ffb_canvas_engine/src/lib.rs:
